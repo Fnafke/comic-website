4619c8c284844a57cc034180671d399448758db1
Fnafke <elin.bachvarov@student.ucll.be>
2023-12-09 01:39:41 +0100

    Introduction hamburger menu fixed

--- Scripts/hamburger_menu.js
+++ Scripts/hamburger_menu.js
@@ -6,5 +6,11 @@ window.onload = () => {
         menu_buttom.classList.toggle("is-active");
         topnav.classList.toggle("is-active");
     })
+
+    const introductionLink = document.querySelector('a[href="#introduction"]');
+    introductionLink.addEventListener("click", function () {
+      // Close the menu
+      topnav.classList.remove("is-active");
+    });
     
 };
